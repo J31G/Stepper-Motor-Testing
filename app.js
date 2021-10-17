efc575e7d19d9a1022890e84fd0feb13605f76c2
Jamie Lambertstock <jamie@lambertstock.com>
2021-10-17 17:33:21 +0100

    Working board movement

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const { Stepper, board } = require('./modules/stepper');
 
-const stepper1 = new Stepper(2, 5, 6400);
+const stepperX = new Stepper(2, 5, 3200);
+const stepperY = new Stepper(3, 6, 3200);
+
+// X = 11.6
+// Y = 7.1
+
+// X = 5.8
+// Y = 3.55
 
 const speed = 1;            // LOWER BEING FASTER
-const steps = 3200;         // AMOUNT OF STEPS TO MOVE
+const stepsX = stepperX.stepsPerRev * 5.8;  // AMOUNT OF STEPS TO MOVE
+const stepsY = stepperY.stepsPerRev * 3.56;
 const waitBetween = 5000;   // AMOUNT OF TIME TO WAIT BETWEEN
+
+// X: (LEFT/RIGHT) = CW: RIGHT, CCW: LEFT
+// Y: TOP/BOTTOM) = CW: DOWN, CCW: TOP
 
 board.on("ready", async () => {
 
-  await stepper1.cw().move(speed, steps, waitBetween);
-  await stepper1.ccw().move(speed, steps, waitBetween);
+  stepperY.ccw().move(speed, stepsY / 2, waitBetween);
+  stepperX.ccw().move(speed, stepsX / 2, waitBetween);
 
 });
